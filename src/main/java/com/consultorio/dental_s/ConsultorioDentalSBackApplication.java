4627666680d39dbb41aaf33f07417d56b399a27e
Dani Castro <castronequizdaniel@outlook.com>
2025-08-22 01:09:15 -0600

    Iniciando el proyecto

---
+++ src/main/java/com/consultorio/dental_s/ConsultorioDentalSBackApplication.java
@@ -0,0 +1,13 @@
+package com.consultorio.dental_s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultorioDentalSBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultorioDentalSBackApplication.class, args);
+	}
+
+}
