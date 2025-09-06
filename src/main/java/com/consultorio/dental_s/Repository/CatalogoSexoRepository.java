7f1b5d9a3b52efe4e56f6da5f9ae02587f715672
Dani Castro <castronequizdaniel@outlook.com>
2025-09-06 15:26:36 -0600

    Se implementa el servicio para agregar, actualizar, eliminar y leer de catalogo de sexo

--- src/main/java/com/consultorio/dental_s/Repository/CatalogoSexoRepository.java
+++ src/main/java/com/consultorio/dental_s/Repository/CatalogoSexoRepository.java
@@ -2,8 +2,18 @@ package com.consultorio.dental_s.Repository;
 
 import com.consultorio.dental_s.Entities.CatalogoSexo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CatalogoSexoRepository extends JpaRepository<CatalogoSexo, Long> {
+
+    @Query("SELECT cs.clave FROM CatalogoSexo cs WHERE cs.clave=:clave")
+    String findByClave(@Param("clave")String clave);
+
+    @Query("SELECT cs FROM CatalogoSexo cs WHERE cs.activo=true")
+    List<CatalogoSexo> findAllCatalogoSexoByActive();
 }
