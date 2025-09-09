b32459ae94047459e953c4d552dd31b86fe01083
Dani Castro <castronequizdaniel@outlook.com>
2025-09-08 23:28:24 -0600

    implementacion del servicio de catalogo tipo sangre

--- src/main/java/com/consultorio/dental_s/Repository/CatalogoTipoSangreRepository.java
+++ src/main/java/com/consultorio/dental_s/Repository/CatalogoTipoSangreRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CatalogoTipoSangreRepository extends JpaRepository<CatalogoTipoSangre, Long> {
+
+    @Query("SELECT ct FROM CatalogoTipoSangre ct WHERE ct.activo=true")
+    List<CatalogoTipoSangre> findAllCatalogoTipoSangre();
 
     @Query("SELECT C FROM CatalogoTipoSangre C WHERE C.cveTipoSangre =:cveTipoSangre OR C.nombreTipoSangre =:nombreTipoSangre")
     Optional<CatalogoTipoSangre> findByName(@Param("cveTipoSangre") String cveTipoSangre, @Param("nombreTipoSangre") String nombreTipoSangre);
